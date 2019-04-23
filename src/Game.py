ebcb492d246424ee76c552b4f5684edcbbef47a5
Vaughn Walker <vaughn.walker@integral.io>
2019-04-22 22:44:59 -0400

    Game of all 1's score a game of 20

--- src/Game.py
+++ src/Game.py
@@ -1,6 +1,9 @@
 class Game:
-    pass
+    def __init__(self):
+        self.current_score = 0
 
+    def score(self):
+        return self.current_score
 
-def score():
-    return 0
+    def roll(self, pins_knocked_down):
+        self.current_score += pins_knocked_down
--- tests/GameTest.py
+++ tests/GameTest.py
@@ -1,9 +1,19 @@
 import unittest
 
-from src import Game
+from src.Game import Game
 
 
 class GameTest(unittest.TestCase):
     def test_game_initializes_with_score_of_zero(self):
-        game = Game
+        game = Game()
         self.assertEqual(0, game.score())
+
+    def test_roll_all_1_score_20(self):
+        game = Game()
+
+        i = 0
+        while i < 20:
+            game.roll(1)
+            i += 1
+
+        self.assertEqual(20, game.score())
