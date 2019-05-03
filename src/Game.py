34ae6ffd795d337a0a0c6762dfd5abd5d0a57a5e
Vaughn Walker <vaughn.walker@integral.io>
2019-05-02 23:02:40 -0400

    Finish kata without frequent commits :(

--- src/Game.py
+++ src/Game.py
@@ -1,9 +1,39 @@
 class Game:
     def __init__(self):
-        self.current_score = 0
+        self.rolls = []
+        self.frames = []
 
     def score(self):
-        return self.current_score
+        total_score = 0
+        roll_index = 0
+
+        for frame_index in range(10):
+            if self.__is_strike(roll_index):
+                total_score += self.__strike_score(roll_index)
+                roll_index += 1
+            elif self.__is_spare(roll_index):
+                total_score += self.__spare_score(roll_index)
+                roll_index += 2
+            else:
+                total_score += self.__roll_frame(roll_index)
+                roll_index += 2
+
+        return total_score
 
     def roll(self, pins_knocked_down):
-        self.current_score += pins_knocked_down
+        self.rolls.append(pins_knocked_down)
+
+    def __roll_frame(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1]
+
+    def __strike_score(self, roll_index):
+        return 10 + self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+
+    def __spare_score(self, roll_index):
+        return 10 + self.rolls[roll_index + 2]
+
+    def __is_strike(self, roll_index):
+        return self.rolls[roll_index] == 10
+
+    def __is_spare(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
--- tests/GameTest.py
+++ tests/GameTest.py
@@ -4,16 +4,37 @@ from src.Game import Game
 
 
 class GameTest(unittest.TestCase):
-    def test_game_initializes_with_score_of_zero(self):
-        game = Game()
-        self.assertEqual(0, game.score())
+    def setUp(self):
+        self.game = Game()
+
+    def test_roll_gutter_game(self):
+        self.__roll_multiple_times(0, 20)
+        self.assertEqual(0, self.game.score())
 
     def test_roll_all_1_score_20(self):
-        game = Game()
+        self.__roll_multiple_times(1, 20)
+        self.assertEqual(20, self.game.score())
 
+    def test_roll_perfect_game_300(self):
+        self.__roll_multiple_times(10, 12)
+        self.assertEqual(300, self.game.score())
+
+    def test_roll_1_strike(self):
+        self.game.roll(10)
+        self.game.roll(1)
+        self.game.roll(1)
+        self.__roll_multiple_times(0, 17)
+        self.assertEqual(14, self.game.score())
+
+    def test_roll_1_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(10)
+        self.__roll_multiple_times(0, 17)
+        self.assertEqual(30, self.game.score())
+
+    def __roll_multiple_times(self, pins, rolls):
         i = 0
-        while i < 20:
-            game.roll(1)
+        while i < rolls:
+            self.game.roll(pins)
             i += 1
-
-        self.assertEqual(20, game.score())
